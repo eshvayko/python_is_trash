a15fe8937cb435e41a1db82b4ebf9a390950a2a3
eshvayko <shvayko.zhenya@gmail.com>
2025-03-12 12:24:21 +0300

    add eugenyx2

--- eugeny.py
+++ eugeny.py
@@ -1,14 +1,15 @@
 import random
 
 letters = []
+addLetters = []
 for i in range(ord('а'), ord('я')+1): # это создание массива буковок
     if chr(i) == 'ж':
         letters.append('ё') # добавление ё
     letters.append(chr(i))
 
-num = random.randint(1,3)
+num = random.randint(1,3) # количество добавленных буковок
+
 i = 0
-addLetters = []
 while i < num:
     addLetters.append(random.choice(letters))
     i += 1
